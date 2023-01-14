5b0de6d27129af54f88f228b836c9f768edc8a14
Mateus Caden <mateuscaden@gmail.com>
2023-01-14 00:19:05 -0300

    Criação do projeto

---
+++ src/main/java/br/com/pessoas/gestaopessoas/GestaopessoasApplication.java
@@ -0,0 +1,13 @@
+package br.com.pessoas.gestaopessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaopessoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaopessoasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pessoas/gestaopessoas/GestaopessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pessoas.gestaopessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaopessoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
